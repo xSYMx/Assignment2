6c3f8497e50813cd8e57550409778a056ba40e5f
Linus Su <87685812+xSYMx@users.noreply.github.com>
2025-04-10 17:27:16 +0800

    .

---
+++ src/models/Technology.java
@@ -0,0 +1,40 @@
+package models;
+
+public abstract class Technology {
+    private double price = 0;
+    private String id = "";
+    private String modelName = "0";
+    private Manufacturer manufacturer;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
